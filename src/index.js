16f324ef0787a7502d8d328db1908c6c16ac2f75
Seshan Ravikumar <seshan10@me.com>
2022-11-27 00:15:44 -0500

    Switch to login with Sineware ID

--- src/index.js
+++ src/index.js
@@ -1,70 +1,83 @@
-import Express from "express";
-import basicAuth from "express-basic-auth";
-import { join, dirname } from 'path';
-import { Low, JSONFile } from 'lowdb';
-import { fileURLToPath } from 'url';
-import dotenv from 'dotenv';
-import got from "got";
-dotenv.config();
+/*
+    Sineware FE Mail Server (Internal Service)
+    Copyright (C) 2022 Seshan Ravikumar
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-const file = join(__dirname, 'db.json');
-const adapter = new JSONFile(file);
-const db = new Low(adapter);
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-await db.read();
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
-db.data = db.data || { emails: [] }
-await db.write();
+require('dotenv').config()
+const Express = require("express");
+const { join } = require('path');
+const got = require("got");
+const { auth, requiresAuth, claimCheck } = require("express-openid-connect");
 
-const app = Express();
-const port = 3002;
+console.log(__dirname)
+async function main() {
+    const { Low, JSONFile } = await import('lowdb');
 
-/**
- * Authorizer for express-basic-auth (use authorizeAsync: true), using username/password
- * @param {string} username
- * @param {string} password
- * @param {function} cb
- * @returns {Promise<void>}
- */
-async function userPassAuthorizer(username, password, cb) {
-    const {body} = await got.post(process.env.AUTHSERVER_URL + '/login', {
-        json: { username, password },
-        responseType: 'json'
-    });
-    console.log(body)
-    cb(null, body.status === true);
-}
+    const file = join(__dirname, 'db.json');
+    const adapter = new JSONFile(file);
+    const db = new Low(adapter);
 
-let authMW = basicAuth({
-    authorizer: userPassAuthorizer,
-    authorizeAsync: true,
-    realm: "femailserver",
-    challenge: true
-});
+    await db.read();
 
-app.use(Express.json({ limit: "50mb" }));
-app.use(Express.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-
-const PREFIX = process.env.PREFIX || '';
-
-app.get(PREFIX + "/", authMW, (req, res) => {
-    res.render('index', {emails: db.data.emails, selectedEmail: false});
-});
-app.get(PREFIX + "/email/:id", authMW, (req, res) => {
-    //console.log(req.params.id)
-    res.render('index', {emails: db.data.emails, selectedEmail: req.params.id});
-});
-
-app.post(PREFIX + "/api/v1/sink", async (req, res) => {
-    console.log(req.body);
-    db.data.emails.push(req.body);
+    db.data = db.data || { emails: [] }
     await db.write();
-    res.send({"success": true, "service": "sineware-fe-mail-server"});
-});
 
-app.listen(port, () => {
-    console.log(`FE Mail Server listening on port ${port}`);
-});
+    const app = Express();
+    const port = 3002;
+
+    app.use(Express.json({ limit: "50mb" }));
+    app.use(Express.urlencoded({extended: true}));
+
+    app.use(auth({
+        authRequired: false,
+        authorizationParams: {
+            response_type: 'id_token',
+            response_mode: 'form_post',
+            scope: 'openid profile email roles'
+        }
+          
+    }))
+
+    app.set('view engine', 'ejs');
+
+    const PREFIX = process.env.PREFIX || '';
+
+
+    const employeeClaim = claimCheck((req, claims) => {
+        console.log(claims)
+        return claims.realm_access?.roles?.includes("employee");
+    })
+
+    app.get(PREFIX + "/", employeeClaim, (req, res) => {
+        res.render('index', {emails: db.data.emails, selectedEmail: false});
+    });
+    app.get(PREFIX + "/email/:id", employeeClaim, (req, res) => {
+        res.render('index', {emails: db.data.emails, selectedEmail: req.params.id});
+    });
+
+    app.post(PREFIX + "/api/v1/sink", async (req, res) => {
+        console.log(req.body);
+        db.data.emails.push(req.body);
+        await db.write();
+        res.send({"success": true, "service": "sineware-fe-mail-server"});
+    });
+
+    app.listen(port, () => {
+        console.log(`FE Mail Server listening on port ${port}`);
+    });
+
+}
+main();
